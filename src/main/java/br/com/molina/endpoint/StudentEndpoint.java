e5abebb1a08989845f9e4fd7fbc39fa80a1630e5
ElaineMolina <elainemolina10@gmail.com>
2022-02-21 09:14:37 -0300

    06 - Padrões REST e POST pt 02

--- src/main/java/br/com/molina/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/molina/endpoint/StudentEndpoint.java
@@ -6,14 +6,7 @@ import br.com.molina.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-import static java.util.Arrays.asList;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("students")
@@ -39,5 +32,9 @@ public class StudentEndpoint {
             return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
-
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Student student){
+        Student.studentList.add(student);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
 }
