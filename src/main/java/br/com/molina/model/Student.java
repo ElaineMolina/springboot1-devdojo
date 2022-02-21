2c0c17fd8918edc026fe70eaee0a83016963eda0
ElaineMolina <elainemolina10@gmail.com>
2022-02-21 08:52:23 -0300

    05 - Padrões REST e POST pt 01

--- src/main/java/br/com/molina/model/Student.java
+++ src/main/java/br/com/molina/model/Student.java
@@ -1,8 +1,24 @@
 package br.com.molina.model;
 
-public class Student {
+import javax.swing.plaf.basic.BasicListUI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
+import static java.util.Arrays.asList;
+
+public class Student {
+    private int id;
     private String name;
+    public static  List<Student> studentList;
+    static{
+        studentRepository();
+    }
+
+    public Student(int id, String name) {
+        this(name);
+        this.id = id;
+    }
 
     public Student(String name) {
         this.name = name;
@@ -11,11 +27,44 @@ public class Student {
     public Student() {
     }
 
+    private static void studentRepository(){
+        studentList = new ArrayList<>(asList(new Student(1,"Deku"),new Student(2,"Todoroki")));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public static void setStudentList(List<Student> studentList) {
+        Student.studentList = studentList;
     }
 }
