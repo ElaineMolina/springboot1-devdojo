209f68b24f7c65b77e5070d91e7b3e2d13a9597d
ElaineMolina <elainemolina10@gmail.com>
2022-03-04 12:30:04 -0300

    15 - Paginação em requisições REST

--- src/main/java/br/com/molina/repository/StudentRepository.java
+++ src/main/java/br/com/molina/repository/StudentRepository.java
@@ -2,9 +2,10 @@ package br.com.molina.repository;
 
 import br.com.molina.model.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface StudentRepository extends CrudRepository <Student, Long>{
+public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
     List<Student> findByNameIgnoreCaseContaining(String name);
 }
